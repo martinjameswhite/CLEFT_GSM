6203bd26f9615bf23d623dcd5979316a4926152e
Stephen Chen <shifan_chen@berkeley.edu>
2018-05-26 13:29:20 -0700

    Some fixes; updated to include options to agree with the fortran code.

--- ps_rsd_python3/velocity_moments.py
+++ ps_rsd_python3/velocity_moments.py
@@ -16,17 +16,8 @@ import sys
 
 class VelocityMoments:
     '''
-    Class to evaluate the Zeldovich power spectrum, given a linear power
-    spectrum k, p [in compatible units, e.g. h/Mpc and (Mpc/h)^3].
-    This can be used as the basis for both the HaloZeldovich and ZEFT
-    approximations, and for auto and cross-spectra.
-    See:
-    Modi, White & Vlah, arXiv:1706.03173
-    https://arxiv.org/abs/1706.03173
-    for more information.
-    To main method is make_table(), which creates the table of power spectra
-    components.  The order is k, ZA, b1, b2, b1sq, b2sq, b1b1
-    Convenience functions are provided for common calls.
+    Class to evaluate the Zeldovich velocity moments.
+    
     '''
     def __init__(self, k, p, f=1):
         '''k,p are the linear theory power spectra in compatible units,
@@ -40,14 +31,14 @@ class VelocityMoments:
         self.tpi2  = 2*np.pi**2.
         self.qt    = np.logspace(-5, 5, 1e4)
         self.kint  = np.logspace(-5, 5, 1e4)
-        self.jn    = 5 #number of bessels to sum over
+        self.jn    = 10 #number of bessels to sum over
         
         # set up velocity table
         self.pktable = None
         self.num_power_components = 3
         
         self.vktable = None
-        self.num_velocity_components = 9
+        self.num_velocity_components = 7
         
         self.sparktable = None
         self.num_spar_components = 5
@@ -251,37 +242,33 @@ class VelocityMoments:
         suppress = np.exp(-0.5*ksq *self.sigma)
 
         # Note: collect all constant offset terms into 'offset'
-        A_const, A_ldep, b1, b1b2, b1sqpb2, b1sq, b1p, offset_za, offset_corlin = 0, 0, 0, 0, 0, 0, 0, 0, 0
+        A, b1, b1b2, b1sqpb2, b1sq, offset_za, offset_corlin = 0, 0, 0, 0, 0, 0, 0
         
         #l indep functions
-        fb1 = 2 * self.Udot
-        fA_const = k * (self.Xdot + self.Ydot - self.sigma)
         fb1b2 = 2 * self.corlin * self.Udot
         
-        foffset_za   = 1 #offset for k*A term
-        foffset_corlin = self.corlin #offset for b1^2 term
+        foffset_za   = k * self.f * self.sigma #offset for k*A term
+        foffset_corlin = k * self.f * self.sigma * self.corlin #offset for b1^2 term
 
         for l in range(self.jn):
             #l-dep functions
-            fA_ldep = k * ( - 2*l/ksq/self.Ylin) * self.Ydot
+            fA = k * (self.Xdot + self.Ydot - self.f * self.sigma) + k * ( - 2*l/ksq/self.Ylin) * self.Ydot
             fb1sqpb2 = 2 * (1 - 2*l/ksq/self.Ylin) * k * self.Ulin * self.Udot
-            fb1sq = k * self.corlin * (self.Xdot + (1 - 2*l/ksq/self.Ylin)*self.Ydot - self.sigma)
-            fb1p = -2 * k**2 * self.Ulin * (self.Xdot + (1 - 2*l/ksq/self.Ylin)*self.Ydot)
+            fb1sq = k * self.corlin * (self.Xdot + (1 - 2*l/ksq/self.Ylin)*self.Ydot - self.f * self.sigma)
+            fb1 = 2 * self.Udot - 2 * k**2 * self.Ulin * (self.Xdot + (1 - 2*l/ksq/self.Ylin)*self.Ydot)
             
             
             #do integrals
             b1 += self.template(k,l,fb1,expon,suppress,power=1)
-            A_const += self.template(k,l,fA_const,expon,suppress,power=0)
-            A_ldep += self.template(k,l,fA_ldep,expon,suppress,power=0)
+            A += self.template(k,l,fA,expon,suppress,power=0)
             b1b2 += self.template(k,l,fb1b2,expon,suppress,power=1)
             b1sqpb2 += self.template(k,l,fb1sqpb2,expon,suppress,power=0)
             b1sq += self.template(k,l,fb1sq,expon,suppress,power=0)
-            b1p += self.template(k,l,fb1p,expon,suppress,power=1) # the other b1 term
             offset_za += self.template(k,l,foffset_za,expon,suppress,power=0,za=True,expon_za=exponm1)
             offset_corlin += self.template(k,l,foffset_corlin,expon,suppress,power=0)
     
         
-        return 4*np.pi*np.array([A_const, A_ldep, b1, b1b2, b1sqpb2, b1sq, b1p, offset_za, offset_corlin])
+        return 4*np.pi*np.array([A, b1, b1b2, b1sqpb2, b1sq, offset_za, offset_corlin])
     
     
     
@@ -299,14 +286,18 @@ class VelocityMoments:
         
         #l indep functions
 
-
-        foffset_za   = -ksq *self.sigma**2 + self.sigma #offset for k*A term
-        #foffset_corlin = self.corlin #offset for b1^2 term
+        # pick the first or second offset based on which A term below
+        foffset_za   = -ksq * self.f**2 * self.sigma**2 + self.f**2 * self.sigma #offset for k*A term
+        foffset_za   = self.f**2 * self.sigma
         
         for l in range(self.jn):
             #l-dep functions
             fb1 = -2 * k * ( (2*self.Udot*self.Xdot + self.Ulin * self.Xdotdot) + (2*self.Udot*self.Ydot + self.Ulin * self.Ydotdot) * (1 - 2*l/ksq/self.Ylin))
+            # keeping all terms present if field quadratic
             fA  = (-ksq*self.Xdot**2+self.Xdot) + (-ksq*2*self.Xdot*self.Ydot+self.Ydotdot)*(1-2*l/ksq/self.Ylin) + -ksq*self.Ydot**2 * (1-4*l/ksq/self.Ylin + 4*l*(l-1)/ksq**2/self.Ylin**2) - foffset_za
+            # for keeping to order P only; pick this to agree with Zvonimir's code
+            fA  = (self.Xdotdot) + (self.Ydotdot)*(1-2*l/ksq/self.Ylin) - foffset_za
+            
             fb1sq = self.corlin*self.Xdotdot + (self.corlin*self.Ydotdot + 2*self.Udot**2)*(1-2*l/ksq/self.Ylin)
             fb2 = 2 * self.Udot**2 * (1-2*l/ksq/self.Ylin)
             
@@ -320,26 +311,33 @@ class VelocityMoments:
         
         return 4*np.pi*np.array([b1,A,b1sq,b2,offset_za])
     
-
+    
     def strace_integrals(self, k):
         '''Do the \mu integrals for various parameters for give 'k' in Tr(\sigma)
             (Commented out sections come from equivalent expressions for density power spectrum.)
-        '''
+            '''
         ksq = k**2
         expon = np.exp(-0.5*ksq * (self.XYlin - self.sigma))
         exponm1 = np.expm1(-0.5*ksq * (self.XYlin - self.sigma))
         suppress = np.exp(-0.5*ksq *self.sigma)
-                
+        
         # Note: collect all constant offset terms into 'offset'
         A, b1,b2, b1sq, offset_za = 0, 0, 0, 0, 0
-                    
+        
         #l indep functions
-        foffset_za   = -ksq * self.f * self.sigma**2 + 3 * self.f**2 * self.sigma #offset for k*A term
-                        
+        # pick the first or second offset based on which A term below
+        foffset_za   = -ksq * self.f**2 * self.sigma**2 + 3 * self.f**2 * self.sigma #offset for k*A term
+        foffset_za = 3 * self.f**2 * self.sigma
+        
         for l in range(self.jn):
             #l-dep functions
             fb1 = -2 * k * ( 2*(self.Xdot+self.Ydot)*self.Udot + self.Ulin*(self.Xdotdot+self.Ydotdot))
+            
+            # keeping all terms present if field quadratic
             fA  = -ksq*self.Xdot**2 + 3*self.Xdotdot + self.Ydotdot - ksq * (self.Ydot**2+2*self.Xdot*self.Ydot)*(1-2*l/ksq/self.Ylin) - foffset_za
+            # for keeping to order P only; pick this to agree with Zvonimir's code
+            fA  =  3*self.Xdotdot + self.Ydotdot - foffset_za #if we keep only to order P
+            
             fb1sq = self.corlin*(3*self.Xdotdot+self.Ydotdot)+2*self.Udot**2
             fb2 = 2 * self.Udot**2
                             
@@ -367,37 +365,53 @@ class VelocityMoments:
 
 
 
-    def make_vtable(self, kmin = 1e-3, kmax = 3, nk = 100):
+    def make_vtable(self, kmin = 1e-3, kmax = 3, nk = 100, ks=None,silent=True):
         '''Make a table of different terms of P(k) between a given
         'kmin', 'kmax' and for 'nk' equally spaced values in log10 of k
         This is the most time consuming part of the code.
         '''
-        self.vktable = np.zeros([nk, self.num_velocity_components+1]) # one column for ks
-        kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+        if ks is not None:
+            kv = ks
+        else:
+            kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+        
+        self.vktable = np.zeros([len(kv), self.num_velocity_components+1]) # one column for ks
         self.vktable[:, 0] = kv[:]
-        for foo in range(nk):
+        for foo in range(len(kv)):
+            if not silent:
+                print(foo)
             self.vktable[foo, 1:] = self.v_integrals(kv[foo])
 
-    def make_spartable(self, kmin = 1e-3, kmax = 3, nk = 100):
+    def make_spartable(self, kmin = 1e-3, kmax = 3, nk = 100, ks=None,silent=True):
         '''Make a table of different terms of P(k) between a given
             'kmin', 'kmax' and for 'nk' equally spaced values in log10 of k
             This is the most time consuming part of the code.
         '''
-        self.sparktable = np.zeros([nk, self.num_spar_components+1]) # one column for ks
-        kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+        if ks is not None:
+            kv = ks
+        else:
+            kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+
+        self.sparktable = np.zeros([len(kv), self.num_spar_components+1]) # one column for ks
         self.sparktable[:, 0] = kv[:]
-        for foo in range(nk):
+        for foo in range(len(kv)):
+            if not silent:
+                print(foo)
             self.sparktable[foo, 1:] = self.spar_integrals(kv[foo])
 
-    def make_stracetable(self, kmin = 1e-3, kmax = 3, nk = 100):
+    def make_stracetable(self, kmin = 1e-3, kmax = 3, nk = 100, ks=None):
         '''Make a table of different terms of P(k) between a given
             'kmin', 'kmax' and for 'nk' equally spaced values in log10 of k
             This is the most time consuming part of the code.
         '''
-        self.stracektable = np.zeros([nk, self.num_strace_components+1]) # one column for ks
-        kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+        if ks is not None:
+            kv = ks
+        else:
+            kv = np.logspace(np.log10(kmin), np.log10(kmax), nk)
+        
+        self.stracektable = np.zeros([len(kv), self.num_strace_components+1]) # one column for ks
         self.stracektable[:, 0] = kv[:]
-        for foo in range(nk):
+        for foo in range(len(kv)):
             self.stracektable[foo, 1:] = self.strace_integrals(kv[foo])
 
 
@@ -424,20 +438,42 @@ class VelocityMoments:
         ff.close()
 
 
-def loglog_abs(x,y,color='k'):
+def loglog_abs(x,y,color='k',label=None):
     '''Routine to make loglog plots where negative values are indicated by dashed lines.
         Mostly for my own convenience.'''
     
-    plt.loglog(x,y,color)
+    plt.loglog(x,y,color, label=label)
     plt.loglog(x,-y,color+'--')
     return None
 
 
 if __name__ == '__main__':
     pks = np.loadtxt('pklin.test')
-    velda = VelocityMoments(pks[:,0],pks[:,1])
-    #velda.make_ptable()
-    #velda.make_vtable()
+    f=1
+
+    velda = VelocityMoments(pks[:,0],pks[:,1],f=f)
+    velda.make_ptable()
+    velda.make_vtable()
     velda.make_spartable()
     velda.make_stracetable()
+
+    #Let's make some plots:
+    #velocity moments (pardon the dirty notation for now):
+    #order of variables: A, b1, b1b2, b1sqpb2, b1sq, offset_za, offset_corlin
+    ks = velda.vktable[:,0]
+    ZA = velda.vktable[:,1] + velda.vktable[:,6]
+    b1 = velda.vktable[:,2]
+    b1sq = velda.vktable[:,4] + velda.vktable[:,5] + velda.vktable[:,7]
+    b2 = velda.vktable[:,4]
+    b1b2 = velda.vktable[:,3]
+
+    loglog_abs(ks,ZA,label='ZA')
+    loglog_abs(ks,b1,color='b',label='b1')
+    loglog_abs(ks,b1sq,color='r',label='b1sq')
+    loglog_abs(ks,b2,color='g',label='b2')
+    loglog_abs(ks,b1b2,color='m',label='b1b2')
+
+    plt.legend()
+    plt.show()
+
 
